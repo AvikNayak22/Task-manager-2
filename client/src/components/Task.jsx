8b9671146fcf93bfed81c18310549ae732ea8395
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2023-12-28 15:54:27 +0530

    Update Task.jsx

--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -4,7 +4,7 @@ import TaskContainer from "./TaskContainer";
 
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:5000/");
+const socket = io.connect("https://taskke-aviknayak22.vercel.app/");
 
 const Task = () => {
   return (
